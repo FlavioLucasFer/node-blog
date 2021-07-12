67eeac724e8ebfceae71f0dd36a629d5a6cdc017
FlavioLucasFer <flaviolukfer@gmail.com>
2021-07-12 15:20:27 -0300

    Edição e exclusão de categorias implementadas
    I implemented edition and deletion of categories

--- routes/admin.js
+++ routes/admin.js
@@ -27,10 +27,6 @@ router.get('/categories/add', (req, res) => {
 	res.render('admin/add_category');
 });
 
-router.get('/posts', (req, res) => {
-	res.send("Admin posts page");
-});
-
 router.post('/categories/new', async (req, res) => {
 	const { name, slug } = req.body;
 	const errors = [];
@@ -60,6 +56,68 @@ router.post('/categories/new', async (req, res) => {
 			res.redirect('/admin');
 		}
 	}
+});
+
+router.get('/categories/edit/:id', async (req, res) => {
+	const { id } = req.params;
+	try {
+		const category = await Category.findOne({_id: id});
+		res.render('admin/edit_category', { 
+			category: category.toJSON(), 
+		});
+	} catch (err) {
+		req.flash('error_msg', "This category does not exits!");
+		res.redirect('/admin/categories');
+	}
+});
+
+router.post('/categories/edit', async (req, res) => {
+	const { id, name, slug } = req.body;
+	const errors = [];
+
+	if (!name) {
+		errors.push({ message: 'Name is mandatory!' });
+	}
+
+	if (!slug) {
+		errors.push({ message: 'Slug is mandatory!' });
+	}
+
+	if (errors.length > 0) {
+		res.render('admin/edit_category');
+	} else {
+		try {
+			const category = await Category.findOne({ _id: id });
+			
+			category.name = name;
+			category.slug = slug;
+			
+			await category.save();
+			
+			req.flash('success_msg', 'Category successfuly edited!');
+			res.redirect('/admin/categories');
+		} catch (err) {
+			req.flash('error_msg', 'An error ocurred when editing category!');
+			res.redirect('/admin/categories');
+		}
+	}
+});
+
+router.post('/categories/delete', async (req, res) => {
+	const { id } = req.body;
+	
+	try {
+		await Category.remove({ _id: id });
+		req.flash('success_msg', 'Category successfully deleted!');
+		res.redirect('/admin/categories');
+	} catch (err) {
+		req.flash('error_msg', 'An error ocurred when deleting category!');
+		res.redirect('/admin/categories');
+	}
+});
+
+router.get('/posts', (req, res) => {
+	res.send("Admin posts page");
 });
 
 module.exports = router;
