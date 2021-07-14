da3bab475d93bdb9130f3535175c20111bf37ea5
FlavioLucasFer <flaviolukfer@gmail.com>
2021-07-14 10:09:09 -0300

    Implemented the CRUD of posts
    I implemented the CRUD of posts including screen to list, add and edit posts. I

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const Post = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	slug: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	contents: {
+		type: String,
+		required: true,
+	},
+	category: {
+		type: Schema.Types.ObjectId,
+		ref: 'categories',
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+model('posts', Post);
+
+module.exports = Post;
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 // Loading modules
 const express = require('express');
-const mongoose = require('mongoose');
+const { model } = require('mongoose');
 require('../models/Category');
+require('../models/Post');
 
 const router = express.Router();
-const Category = mongoose.model('categories');
+const Category = model('categories');
+const Post = model('posts');
 
 // Routes
 router.get('/', (req, res) => {
@@ -116,8 +118,146 @@ router.post('/categories/delete', async (req, res) => {
 	}
 });
 
-router.get('/posts', (req, res) => {
-	res.send("Admin posts page");
+router.get('/posts', async (req, res) => {
+	try {
+		const posts = await Post.find().populate('category').sort({date: 'desc'});
+		res.render('admin/posts', { posts: posts.map(e => e.toJSON()) });
+	} catch (err) {
+		req.flash('error_msg', 'An error ocurred when listing posts');
+		res.redirect('/admin');
+	}
+});
+
+router.get('/posts/add', async (req, res) => {
+	try {
+		const categories = await Category.find();
+		res.render('admin/add_post', { categories: categories.map(e => e.toJSON()) });
+	} catch (err) {
+		req.flash('error_msg', 'An error ocurred when load form');
+		res.redirect('/admin');
+	}
+});
+
+router.post('/posts/new', async (req, res) => {
+	const { 
+		title, 
+		slug, 
+		description,
+		contents,
+		category, 
+	} = req.body;
+
+	let errors = [];
+
+	if (!title) 
+		errors.push({message: 'Title is mandatory!'});
+	
+	if (!slug)
+		errors.push({message: 'Slug is mandatory!'});
+	
+	if (!description)
+		errors.push({message: 'Description is mandatory!'});
+
+	if (!contents)
+		errors.push({message: 'Contents is mandatory!'});
+
+	if (category === '0') 
+		errors.push({message: 'Invalid category, register a category!'});
+
+	if (errors.length > 0) {
+		res.render('admin/add_post', { errors });
+	} else {
+		const newPost = {...req.body};
+
+		try {
+			await new Post(newPost).save();
+			req.flash('success_msg', 'New post successfully created!');
+			res.redirect('/admin/posts');
+		} catch (err) {
+			req.flash('error_msg', 'An error ocurred when saving post!');
+			res.redirect('/admin/posts');
+		}
+	}
+});
+
+router.get('/posts/edit/:id', async (req, res) => {
+	const { id } = req.params;
+	
+	try {
+		const post = await Post.findOne({_id: id })
+		const categories = await Category.find();
+		res.render('admin/edit_post', {
+			post: post.toJSON(),
+			categories: categories.map(e => e.toJSON()),
+		});
+	} catch (err) {
+		req.flash('error_msg', 'An error ocurred when editing post!');
+		res.redirect('/admin');
+	}
+});
+
+router.post('/posts/edit', async (req, res) => {
+	const {
+		id,
+		title,
+		slug,
+		description,
+		contents,
+		category,
+	} = req.body;
+
+	let errors = [];
+
+	if (!title)
+		errors.push({ message: 'Title is mandatory!' });
+
+	if (!slug)
+		errors.push({ message: 'Slug is mandatory!' });
+
+	if (!description)
+		errors.push({ message: 'Description is mandatory!' });
+
+	if (!contents)
+		errors.push({ message: 'Contents is mandatory!' });
+
+	if (category === '0')
+		errors.push({ message: 'Invalid category, register a category!' });
+
+	if (errors.length > 0) {
+		res.render('admin/add_post', { errors });
+	} else {
+		try {
+			const post = await Post.findOne({ _id: id })
+			
+			post.title = title;
+			post.slug = slug;
+			post.description = description;
+			post.contents = contents;
+			post.category = category;
+
+			await post.save();
+
+			req.flash('success_msg', 'Post successfully edited!');
+			res.redirect('/admin/posts');
+		} catch (err) {
+			req.flash('error_msg', 'An error ocurred when saving edit');
+			res.redirect('/admin/posts');
+		}
+	}
+});
+
+router.get('/posts/delete/:id', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		await Post.remove({_id: id});
+
+		req.flash('success_msg', 'Post successfully deleted!');
+		res.redirect('/admin/posts');
+	} catch (err) {
+		req.flash('error_msg', 'An error ocurred when deleting post!');
+		res.redirect('/admin/posts');
+	}
 });
 
 module.exports = router;
