09b1eab90a032a6fe003b6119913fc61fb18fa9b
FlavioLucasFer <flaviolukfer@gmail.com>
2021-07-15 09:27:13 -0300

    Home page with posts list
    I implemented the home page with post list and individual post page for each post

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
 
+// Loading models
+require('./models/Post');
+const Post = mongoose.model('posts');
+
 const app = express();
 
 // Configuration
@@ -45,6 +49,37 @@ app.use(express.static(path.join(/*__dirname stores the absolute path of the pro
 
 // Routes
 const admin = require('./routes/admin');
+
+app.get('/', async (req, res) => {
+	try {
+		const posts = await Post.find().populate('category').sort({date: 'desc'});
+		res.render('index', { posts: posts.map(e => e.toJSON()) });
+	} catch (err) {
+		req.flash('error_msg', 'An internal error ocurred!');
+		res.redirect('/404');
+	}
+});
+
+app.get('/post/:slug', async (req, res) => {
+	const { slug } = req.params;
+	try {
+		const post = await Post.findOne({slug})
+		
+		if (post) res.render('post/index', {post: post.toJSON()});
+		else {
+			req.flash('error_msg', 'This post does not exists!');
+			res.redirect('/');
+		}
+
+	} catch (err) {
+		req.flash('error_msg', 'An internal error ocurred!');
+		res.redirect('/');
+	}
+});
+
+app.get('/404', (req, res) => {
+	res.send('Error: 404!');
+});
 
 app.use('/admin', admin);
 
